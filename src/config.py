f637903509bbbbc6788dda664c9b9741f24c0ffb
home computer <mhy5413@naver.com>
2022-08-03 01:27:09 +0900

    config 파일 수정

--- src/config.py
+++ src/config.py
@@ -1,4 +1,19 @@
 # 설정 파일 관리
+import configparser
+import os
+
+USER_CFG="user.cfg"
+
 class Config:
     def __init__(self):
-        pass
+        config=configparser.ConfigParser()
+        config['DEFAULT']={
+            "used_coin":"BTC/USDT",
+            "buy_timeout":"0",
+            "sell_timeout":"0"
+        }
+        
+        if not os.path.exists(USER_CFG):
+            print("There is no user.cfg file. Check it out!")
+        else:
+            config.read(USER_CFG)
