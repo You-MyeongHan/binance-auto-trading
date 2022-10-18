ffff3673b36cff28a4904fce093132c218f2d614
You Myeonghan <mhy5413@gmail.com>
2022-10-18 22:51:29 +0900

    modify basic folder

--- src/database.py
+++ src/database.py
@@ -7,11 +7,17 @@ import pymysql
 from .config import Config
 from .logger import Logger
 
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session, sessionmaker
+
 class Database:
-    def __int__(self, logger: Logger, config: Config):
+    def __int__(self, logger: Logger, config: Config, uri=""):
         self.logger = logger
         self.config = config
-    
+        self.engine = create_engine(uri)
+        self.SessionMaker = sessionmaker(bind=self.engine)
+
+
     def create_database(self):
         pass
     
---
+++ test.py
@@ -0,0 +1,5 @@
+from ast import While
+from tkinter import W
+from tokenize import Whitespace
+import numpy as np
+
